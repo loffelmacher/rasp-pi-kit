add08b4ad83f7ddb02468b41d528d686fb66c84d
Randy L <randy@loffelmacher.com>
2021-02-28 22:22:52 -0500

    done with Doorbell

--- 06.1.1_Doorbell/Doorbell.py
+++ 06.1.1_Doorbell/Doorbell.py
@@ -10,28 +10,32 @@ import RPi.GPIO as GPIO
 buzzerPin = 11    # define buzzerPin
 buttonPin = 12    # define buttonPin
 
+
 def setup():
     GPIO.setmode(GPIO.BOARD)        # use PHYSICAL GPIO Numbering
     GPIO.setup(buzzerPin, GPIO.OUT)   # set buzzerPin to OUTPUT mode
-    GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # set buttonPin to PULL UP INPUT mode
+    # set buttonPin to PULL UP INPUT mode
+    GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 
 def loop():
     while True:
-        if GPIO.input(buttonPin)==GPIO.LOW: # if button is pressed
-            GPIO.output(buzzerPin,GPIO.HIGH) # turn on buzzer
-            print ('buzzer turned on >>>')
-        else : # if button is relessed
-            GPIO.output(buzzerPin,GPIO.LOW) # turn off buzzer
-            print ('buzzer turned off <<<')
+        if GPIO.input(buttonPin) == GPIO.LOW:  # if button is pressed
+            GPIO.output(buzzerPin, GPIO.HIGH)  # turn on buzzer
+            print('buzzer turned on >>>')
+        else:  # if button is relessed
+            GPIO.output(buzzerPin, GPIO.LOW)  # turn off buzzer
+            print('buzzer turned off <<<')
+
 
 def destroy():
     GPIO.cleanup()                     # Release all GPIO
 
+
 if __name__ == '__main__':     # Program entrance
-    print ('Program is starting...')
+    print('Program is starting...')
     setup()
     try:
         loop()
     except KeyboardInterrupt:  # Press ctrl-c to end the program.
         destroy()
-
